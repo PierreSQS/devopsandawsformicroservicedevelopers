2c7b0ee12c1bd2cd893ef7fca2a27649813bfe30
PierreSQS <nomo_messina@web.de>
2023-06-28 08:10:58 +0200

    Sec15_Chap152-Launch and Test: upgraded couponservice code to SB 3.1.1

--- couponservice/src/main/java/com/bharath/springcloud/controllers/CouponRestController.java
+++ couponservice/src/main/java/com/bharath/springcloud/controllers/CouponRestController.java
@@ -1,15 +1,9 @@
 package com.bharath.springcloud.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import com.bharath.springcloud.model.Coupon;
 import com.bharath.springcloud.repos.CouponRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/couponapi")
@@ -18,14 +12,14 @@ public class CouponRestController {
 	@Autowired
 	CouponRepo repo;
 
-	@RequestMapping(value = "/coupons", method = RequestMethod.POST)
+	@PostMapping(value = "/coupons")
 	public Coupon create(@RequestBody Coupon coupon) {
 
 		return repo.save(coupon);
 
 	}
 
-	@RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+	@GetMapping(value = "/coupons/{code}")
 	public Coupon getCoupon(@PathVariable("code") String code) {
 		return repo.findByCode(code);
 
